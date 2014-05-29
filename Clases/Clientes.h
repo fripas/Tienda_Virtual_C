716d63084b0f796f8519d5395fc7a47e58992231
MartinMol <martin.mol.rhz@gmail.com>
2014-05-29 12:01:34 -0500

    Clase Clientes
    Agregué la clase Clientes

---
+++ Clases/Clientes.h
@@ -0,0 +1,12 @@
+#ifndef CLIENTES_H_INCLUDED
+#define CLIENTES_H_INCLUDED
+
+class Clientes{
+private:
+    int id,hola;
+public:
+    Clientes();
+    Clientes(Clientes &);
+};
+
+#endif // CLIENTES_H_INCLUDED
